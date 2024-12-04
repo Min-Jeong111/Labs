f61971081bffb3620717106e7491ddec335ff8c9
MinJeong111 <MinJeong111@gmail.com>
2024-12-04 15:39:28 +0200

    Modified lab.4 part 2

--- Lab.4-2.cpp
+++ Lab.4-2.cpp
@@ -3,71 +3,76 @@
 // Ex.5
 struct FruitCounts
 {
-   int apples;
-   int pears;
-   int oranges;
+	int apples;
+	int pears;
+	int oranges;
 };
+
 int main()
 {
-    int apples;
-    int pears;
-    int oranges;
+	int apples;
+	int pears;
+	int oranges;
+	
+	FruitCounts fruitCounts;
 
-    FruitCounts fruitCounts;
+	std::cout << "Apples:";
+	std::cin >> fruitCounts.apples;
 
-    std::cout << "Apples:";
-    std::cin >> fruitCounts.apples;
+	std::cout << "Pears:";
+	std::cin >> fruitCounts.pears;
 
-    std::cout << "Pears:";
-    std::cin >> fruitCounts.pears;
+	std::cout << "Oranges:";
+	std::cin >> fruitCounts.oranges;
+	
+	// Ex.1
+	if(apples > 5 && pears < 8 && oranges * 2 > apples)
+	{
+		std::cout << "Hello" << std::endl;
+	}
 
-    std::cout << "Oranges:";
-    std::cin >> fruitCounts.oranges;
+	// Ex.2
+	bool result = apples > 5 && pears < 8 && oranges * 2 > apples; // Поменял с int на bool
+	if(result)
+	{
+		std::cout << "Hello" << std::endl;
+	}
 
-    // Ex.1
-    if(apples > 5 && pears < 8 && oranges * 2 > apples)
-    {
-	std::cout << "Hello" << std::endl;
-    }
+	// Ex.3
+	bool checkApples = apples > 5;
+	bool checkPears = pears < 8;
+	bool checkOranges = oranges * 2 > apples;
+	bool checkResult = checkApples && checkPears && checkOranges;
 
-    // Ex.2
-    int result = apples > 5 && pears < 8 && oranges * 2 > apples;
-    if(result)
-    {
-	std::cout << "Hello" << std::endl;
-    }
+	if(checkResult)
+    	{
+		std::cout << "Hello" << std::endl;
+    	}
 
-    // Ex.3
-    bool checkApples = apples > 5;
-    bool checkPears = pears < 8;
-    bool checkOranges = oranges * 2 > apples;
-    bool checkResult = checkApples && checkPears && checkOranges;
-    if(checkResult)
-    {
-	std::cout << "Hello" << std::endl;
-    }
+    	// Ex.4
+    	bool result1; // Объявил вне блока булевую переменную, которую я инициализирую ниже со всеми условиями
+    	{
+		result1 = checkApples && checkPears && checkOranges;
 
-    // Ex.4
-    {
-	 bool checkApples = apples > 5;
-    	 bool checkPears = pears < 8;
-    	 bool checkOranges = oranges * 2 > apples;
+		bool checkApples = apples > 5;
+    		bool checkPears = pears < 8;
+	    	bool checkOranges = oranges * 2 > apples;
 
-	 if(checkApples && checkPears && checkOranges)
-    	 {
-	     std::cout << "Hello" << std::endl;
-    	 }
-    }
-
-    {
-	 bool checkApples = fruitCounts.apples > 5;
-    	 bool checkPears = fruitCounts.pears < 8;
-    	 bool checkOranges = fruitCounts.oranges * 2 > fruitCounts.apples;
-	 if(checkApples && checkPears && checkOranges)
-    	 {
-	     std::cout << "Hello" << std::endl;
-    	 }
-    }
-
-   return 0;
+		if(result1)
+    		{
+	    		std::cout << "Hello" << std::endl;
+    		}
+    	}
+	
+	{
+	  bool checkApples = fruitCounts.apples > 5;
+    	  bool checkPears = fruitCounts.pears < 8;
+    	  bool checkOranges = fruitCounts.oranges * 2 > fruitCounts.apples;
+	  if(checkApples && checkPears && checkOranges)
+    	  {
+		std::cout << "Hello" << std::endl;
+    	  }
+	}
+	// Насчёт отступов, у меня, вроде бы, тут где я написал код, есть отступы и всё выглядит как должно. Во всех блоках есть отступы.
+	return 0;
 }
